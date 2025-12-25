cd4cd7262dab94a39f71447bb27331c7ff930f6d
0xr3ngar <bogdan.nikolov4@Outlook.com>
2025-12-25 23:42:49 +0200

    feat: add get user input

--- src/main.c
+++ src/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  // Flush after every printf
+char *get_input(FILE *fp, size_t size) {
+  char *str;
+  int ch;
+  size_t len = 0;
+  str = realloc(NULL, sizeof(*str) * size);
+  if (!str)
+    return str;
+
+  while (EOF != (ch = fgetc(fp)) && ch != '\n') {
+    str[len++] = ch;
+    if (len == size) {
+      str = realloc(str, sizeof(*str) * (size += 16));
+      if (!str)
+        return str;
+    }
+  }
+  str[len++] = '\0';
+
+  return realloc(str, sizeof(*str) * len);
+}
+
+int main(void) {
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
+  char *userInput;
+
   printf("$ ");
 
+  userInput = get_input(stdin, 20);
+
+  printf("%s: command not found\n", userInput);
+
+  free(userInput);
   return 0;
 }
